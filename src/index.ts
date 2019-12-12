cbee8f99b429b93af5be8b8957323f439ba73a70
piglovesyou <thepiglovesyou@gmail.com>
2019-12-12 13:19:24 +0900

    Use "loadSchema" of graphql-toolkit to use lower level functions

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,46 @@
+import { promises as fsPromises } from "fs";
 import { loader } from 'webpack';
-import { promises as fsPromises, mkdirSync } from "fs";
-import { getOptions } from 'loader-utils';
-import path from "path";
-import { generate } from "@graphql-codegen/cli";
-import genDts from "./gen-dts";
-import merge from 'lodash.merge';
-
 import mkdirp from "mkdirp";
-const { writeFile } = fsPromises;
+import { getOptions } from 'loader-utils';
+import path from 'path';
+import { codegen } from "@graphql-codegen/core";
+import * as typescriptPlugin from '@graphql-codegen/typescript';
+import * as typescriptOperationsPlugin from '@graphql-codegen/typescript-operations';
+import * as typescriptReactApolloPlugin from '@graphql-codegen/typescript-react-apollo';
+import genDts from "./gen-dts";
+import { DocumentNode, GraphQLSchema, parse, printSchema } from "graphql";
+import {loadSchema} from "graphql-toolkit";
 
+const { writeFile } = fsPromises;
 const libDir = path.resolve(__dirname, '..');
 const tsxBaseDir = path.join(libDir, '__generated__');
 mkdirp.sync(tsxBaseDir);
 
+const defaultCodegenConfig = {
+  config: {
+    // withHOC: false,  // True by default
+    withHooks: true,    // False by default
+  },
+  plugins: [
+    { typescript: {} },
+    { 'typescript-operations': {} },
+    { 'typescript-react-apollo': {} },
+  ],
+  pluginMap: {
+    typescript: typescriptPlugin,
+    'typescript-operations': typescriptOperationsPlugin,
+    'typescript-react-apollo': typescriptReactApolloPlugin,
+  },
+};
+
 const graphlqCodegenLoader = async function (this: loader.LoaderContext, gqlContent: string) {
-  const options = getOptions(this);
+  const options = getOptions(this) as any;
   const callback = this.async()!;
 
   const { resourcePath: gqlFullPath, rootContext: userDir } = this;
 
   const gqlRelPath = path.relative(userDir, gqlFullPath);
-  const tsxRelPath = gqlRelPath +  `.tsx`;
+  const tsxRelPath = gqlRelPath + `.tsx`;
   const tsxFullPath = path.join(tsxBaseDir, tsxRelPath);
   const dtsFullPath = `${ gqlFullPath }.d.ts`;
 
@@ -28,30 +48,23 @@ const graphlqCodegenLoader = async function (this: loader.LoaderContext, gqlCont
   // babel-loader at least doesn't respond the .graphql extension.
   this.resourcePath = `${ gqlFullPath }.tsx`;
 
-  const [ { content: tsxContent } ] = await generate(
-      merge(
-          {
-            config: {
-              // withHOC: false,  // True by default
-              withHooks: true,    // False by default
-            },
-          },
-          options,
-          {
-            documents: gqlContent,
-            generates: {
-              [tsxFullPath]: {
-                plugins: [
-                  'typescript',
-                  'typescript-operations',
-                  'typescript-react-apollo',
-                ],
-              },
-            },
-          }
-      ) ,
-      true,
-  );
+  // TODO: Memoize
+  const loadedSchema: GraphQLSchema = await loadSchema(options.schema);
+  const schema: DocumentNode = parse(printSchema(loadedSchema));
+
+  const codegenConfig = {
+    ...defaultCodegenConfig,
+    ...options,
+    schema,
+    filename: tsxFullPath,
+    documents: [
+      {
+        filePath: gqlFullPath,
+        content: parse(gqlContent),
+      }
+    ],
+  };
+  const tsxContent = await codegen(codegenConfig);
 
   const dtsContent = await genDts(tsxFullPath);
   await writeFile(dtsFullPath, dtsContent);
